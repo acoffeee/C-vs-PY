fcb86c6309b1f096f2d89a4e9f2fafba0a7f4e6b
coffeee <71660795+acoffeee@users.noreply.github.com>
2025-05-09 11:12:24 -0400

    Create guess.py

---
+++ guess.py
@@ -0,0 +1,10 @@
+import random
+answer = random.range(0,10)
+User_choice = 0
+while user_choice != answer:
+  user_choice = int(input("guess 1-10")
+  if user_choice == answer:
+      print("you got it!")
+      continue
+else:
+  
